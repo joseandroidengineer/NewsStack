b78097fab7506ed7e1edcb1c0d25a1453969b2de
Jose Escalante <joseandroiddeveloper@gmail.com>
2022-11-22 07:34:09 -0500

    -Had to a comment to the CustomTestRunner.kt

--- app/src/test/java/com/jge/newsstack/CustomTestRunner.kt
+++ app/src/test/java/com/jge/newsstack/CustomTestRunner.kt
@@ -4,7 +4,13 @@ import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltTestApplication
-
+/**I was confused here since I know I had to use HiltDagger to try and inject my dependencies
+ * it was mentioned that I had to use a CustomTestRunner so that it could work. However, I ran into
+ * compilation issues and errors. I thought I had to add this class in the test package, but then
+ * I doubted myself and thought it was in the androidTest directory.
+ * I feel like this was my roadblock in completing the tests and
+ * I'm on the right direction, I just need a hint or 2 to figure it out.
+ * **/
 class CustomTestRunner:AndroidJUnitRunner() {
 
     override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
